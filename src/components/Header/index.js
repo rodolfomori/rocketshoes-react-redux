60a467ed78ea67c405c446ed6c053aa0d85d9685
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-05-01 11:01:48 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="logo rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
